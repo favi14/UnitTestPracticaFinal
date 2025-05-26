6ab95cc884963da493364c5e72aa3739c2b7f8ad
favi14 <puyalolivafaviola@gmail.com>
2025-05-25 21:35:00 -0400

    Final steps of practice.

--- PracticaFinal.Tests/Controllers/StudentControllerTests.cs
+++ PracticaFinal.Tests/Controllers/StudentControllerTests.cs
@@ -1,10 +1,33 @@
-﻿namespace PracticaFinal.Tests;
+﻿using PracticaFinal.Controllers;
+using PracticaFinal.Models;
+using PracticaFinal.Services;
+using Moq;
 
-public class UnitTest1
+namespace PracticaFinal.Tests
 {
-    [Fact]
-    public void Test1()
+    public class StudentControllerTests
     {
+        [Fact]
+        public void Student_withValidCIAndOver18Age_IsAprob()
+        {
+            // Arrange
+            var studentService = new Mock<IStudentService>();
+            StudentController studentController = new StudentController(studentService.Object);
+            // var student = new Student { CI = 1, Nombre = "Alice", Nota = 60 };
+            studentService.Setup(s => s.IsAprob(1)).Returns(
+                true
+            );
 
+            /*
+            StudentService studentService = new StudentService();
+            StudentController studentController = new StudentController(studentService);
+            */
+
+            // Act
+            Boolean result = studentController.EvaluateStudentIsAprob(1);
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
